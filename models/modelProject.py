f55f1d697bc0c2e25404995ddfa665c147fe928a
mectoys <mectoys2013@gmail.com>
2024-02-25 08:42:34 -0500

    Segunda  parte proyecto Final donde agrego modulos y rutas para el funcionamiento del CRUD y ventana emergentes con SWEETALERT2.

--- models/modelProject.py
+++ models/modelProject.py
@@ -1,4 +1,4 @@
-from database.connectDB import  get_connection_SQL_SERVER
+from database.connectDB import get_connection_SQL_SERVER
 
 
 class modelProject:
@@ -7,11 +7,57 @@ class modelProject:
         connection = get_connection_SQL_SERVER()
         with connection.cursor() as cursor:
             try:
-                query="SELECT IDBCO, DESCRIPCION FROM BANCOS"
+                query = "SELECT IDBCO, DESCRIPCION FROM BANCOS"
                 cursor.execute(query)
-                result= cursor.fetchall()
-                return  result
+                result = cursor.fetchall()
+                return result
 
             except Exception as e:
-                print(f"Error en la consulta { str(e)}")
+                print(f"Error en la consulta {str(e)}")
                 return []
+
+    # Insert
+    @staticmethod
+    def insert_bank(description):
+        connection = get_connection_SQL_SERVER()
+        mycursor = connection.cursor()
+        SQLINSERT = "INSERT INTO Bancos (DESCRIPCION) VALUES(?)"
+        try:
+            mycursor.execute(SQLINSERT, description)
+        except:
+            connection.rollback()
+        else:
+            connection.commit()
+        finally:
+            connection.close()
+            return True
+
+    @staticmethod
+    def update_bank(idbank, description):
+        connection = get_connection_SQL_SERVER()
+        mycursor = connection.cursor()
+        SQLUPDATE = "UPDATE Bancos SET DESCRIPCION=? WHERE  IDBCO=?"
+        try:
+            mycursor.execute(SQLUPDATE, description, idbank)
+        except:
+            connection.rollback()
+        else:
+            connection.commit()
+        finally:
+            connection.close()
+            return True
+
+    @staticmethod
+    def delete_bank(idbank):
+        connection = get_connection_SQL_SERVER()
+        mycursor = connection.cursor()
+        SQLDELETE = "DELETE FROM Bancos WHERE IDBCO=?"
+        try:
+            mycursor.execute(SQLDELETE, idbank)
+        except:
+            connection.rollback()
+        else:
+            connection.commit()
+        finally:
+            connection.close()
+            return True
--- views/viewProject.py
+++ views/viewProject.py
@@ -8,3 +8,31 @@ main = Blueprint("datatable_bp", __name__)
 def getBanks():
     banks = modelProject.get_dataBank()
     return render_template('/mantenimiento.html', banks=banks)
+
+
+@main.route('/insert', methods=['POST', 'GET'])
+def insertBank():
+    if request.method == "POST":
+        data = request.get_json()
+        descripcion = data.get('bank', None)
+        modelProject.insert_bank(descripcion)
+    return "OK"
+
+
+@main.route('/update', methods=['POST', 'GET'])
+def updateBank():
+    if request.method == "POST":
+        data = request.get_json()
+        idbco = data.get('idbco', None)
+        descripcion = data.get('bank', None)
+        modelProject.update_bank(idbco, descripcion)
+    return "OK"
+
+
+@main.route('/delete', methods=['POST', 'GET'])
+def deleteBank():
+    if request.method == "POST":
+        data = request.get_json()
+        idbco = data.get('idbco', None)
+        modelProject.delete_bank(idbco)
+    return "OK"
